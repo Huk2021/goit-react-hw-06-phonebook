f7514f030102e2f0469d9f9aab38e81c18b41ae6
Viktoriia Huk <relax16011991@gmail.com>
2022-02-18 18:03:59 +0200

    Update ContactsList.js

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,9 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useSelector, useDispatch } from "react-redux";
+import {deleteContact} from '../../redux/actions'
 import ElementContactsList from "../ElementContactsList/ElementContactsList";
 import { ListContact, ButtonContact } from "./ContactsList.styled";
 
-export default function ContactsList({ contacts, onDeleteContact }) {
+export default function ContactsList() {
+  const dispatch = useDispatch();
+  const onDeleteContact = (id) => dispatch(deleteContact(id));
+  
+  const getContacts = (state) => {
+    const { filter, items } = state.contacts;
+    const normalizedFilter = filter.toLowerCase();
+
+    return items.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  const contacts = useSelector(getContacts);
+  
   return (
     <ListContact>
       {contacts.map(({ id, name, number }) => (
