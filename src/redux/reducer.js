59b0b8e8aaa27428995aa0f2b00ba82afa6c3f5e
Viktoriia Huk <relax16011991@gmail.com>
2022-02-19 16:56:49 +0200

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,10 +4,17 @@ import {addContact, deleteContact, changeFilter} from './actions';
 
 
 const items = createReducer([], {
-  [addContact]: (state, { payload }) => [...state, payload],
-  [deleteContact]: (state, { payload }) =>
+    [addContact]: (state, { payload }) =>{
+    if (state.find(
+        contact => contact.name.toLowerCase() === payload.name.toLowerCase())
+    ) {
+        alert(`${payload.name} is already in contacts.`);
+        return;
+    }
+        return [...state, payload];
+    },
+    [deleteContact]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
-  
 });
 
 const filter = createReducer('', {
